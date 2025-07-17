f89f4d7d210cce95d539040164a0eb9b49a05767
amr roshdy shanab <120336819+am172@users.noreply.github.com>
2025-07-17 19:44:24 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,34 +7,45 @@ import Services from "./Sections/Services";
 import Testimonalis from "./Sections/Testimonails";
 import Footer from "./Sections/Footer";
 import Loader from "./Sections/Loader";
+import ProjectDetails from "./Sections/Projects/ProjectDetails"; 
+
 import { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 const App = () => {
   const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     const timeout = setTimeout(() => {
       setLoading(false);
     }, 1800);
-
     return () => clearTimeout(timeout);
-  }, [])
+  }, []);
 
   return (
-    <>
+    <Router>
       {loading && <Loader />}
       {
         !loading &&
         <>
           <Navbar />
-          <Header />
-          <About />
-          <Services />
-          <Projects />
-          <Testimonalis />
-          <Contact />
-          <Footer />
+          <Routes>
+            <Route path="/" element={
+              <>
+                <Header />
+                <About />
+                <Services />
+                <Projects />
+                <Testimonalis />
+                <Contact />
+                <Footer />
+              </>
+            } />
+            <Route path="/project/:id" element={<ProjectDetails />} />
+          </Routes>
         </>
       }
-    </>
+    </Router>
   );
 };
 
