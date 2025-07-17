9f1380897ef9e99da316f5a5c8adc095eb5371b4
amr roshdy shanab <120336819+am172@users.noreply.github.com>
2025-07-17 19:46:40 +0300

    Update index.ts

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,20 +1,97 @@
 export {default as logo} from "./logo.svg"
 export {default as amr} from "./amr.png"
 
-// export {default as project1} from "./project1.png"
-// export {default as project2} from "./project2.png"
-// export {default as project3} from "./project3.png"
-// export {default as project4} from "./project4.png"
+
 export {default as dall} from "./DALL·E 2.jpg"
-export {default as note} from "./note.png"
-export {default as blog} from "./Capture3.jpg"
-export {default as zekr} from "./dekr2.jpg"
-export {default as ecom} from "./ecpommers.jpg"
-export {default as proto} from "./proto.jpg"
+export {default as newPhoto} from "./photo_2025-03-12_04-20-46-removebg-preview.png"
+export {default as note} from "./taskat.jpg"
+export { default as blog } from "./Capture3.jpg"
+
+//zekr
+export { default as zekr } from "./zkr.png"
+export { default as zekr1 } from "./home.jpeg"
+export { default as zekr2 } from "./album.jpeg"
+export { default as zekr3 } from "./admin_playlist.jpeg"
+export { default as zekr4 } from "./admin_song.jpeg"
+
+//fadaa
+export {default as fadaa} from "./fadaa.png"
+export { default as fadaa1 } from "./fadaa1.jpeg"
+export { default as fadaa2 } from "./fadaa2.jpeg"
+export { default as fadaa3 } from "./fadaa3.jpeg"
+export { default as fadaa4 } from "./fadaa4.jpeg"
+export { default as fadaa5 } from "./fadaa5.jpeg"
+export { default as fadaa6 } from "./fadaa6.jpeg"
+export { default as fadaa7 } from "./dash1.jpeg"
+export { default as fadaa8 } from "./dash2.jpeg"
+export { default as fadaa9 } from "./dash4.jpeg"
+export { default as fadaa10 } from "./dash6.jpeg"
+
+//arabia
+export {default as arb1} from "./arb1.jpeg"
+export {default as arb2} from "./arb2.jpeg"
+export {default as arb3} from "./arb3.jpeg"
+export {default as arb4} from "./arb4.jpeg"
+export {default as arb5} from "./arb5.jpeg"
+export {default as arb6} from "./arb6.jpeg"
+export {default as arb7} from "./arb7.jpeg"
+export {default as arb8} from "./arb8.jpeg"
+export {default as about} from "./about.jpeg"
+export {default as contact} from "./contact.jpeg"
+export {default as comp} from "./comp.jpeg"
+export {default as product} from "./product.jpeg"
+export {default as metal} from "./metal.jpeg"
+export { default as cate } from "./cate.jpeg"
+export { default as dash } from "./dash1.jpeg"
+export { default as dash1 } from "./dash2.jpeg"
+export { default as dash2 } from "./dash3.jpeg"
+export { default as dash4 } from "./dash4.jpeg"
+
+
+//fatwa 
+export { default as fatwa } from "./20250525_1330_واجهة موقع FatwaAI_remix_01jw3ft261fj9vpq769qq8ag7w.png"
+export { default as fatwa1 } from "./Screenshot_25-5-2025_134622_fatwa-ai-inquire.lovable.app.jpeg"
+export { default as fatwa2 } from "./Screenshot_25-5-2025_13463_fatwa-ai-inquire.lovable.app.jpeg"
+export { default as fatwa3 } from "./Capture8.jpeg"
+
+//DAL
+export { default as dal1 } from "./dal1.jpeg"
+export { default as dal2 } from "./dal2.jpeg"
+export { default as dal3 } from "./dal3.jpeg"
+export { default as dal4 } from "./dal4.jpeg"
+export { default as dal5 } from "./dal5.jpeg"
+export { default as dal6 } from "./dal6.jpeg"
+export { default as dal7 } from "./dal7.jpeg"
+export { default as dal8 } from "./dal8.jpeg"
+export { default as dal9 } from "./dal9.jpeg"
+export { default as dal10 } from "./dal10.jpeg"
+
+//BAHAAA
+export { default as bahaa } from "./ChatGPT Image May 23, 2025, 04_26_02 PM.png"
+export { default as bahaa1 } from "./elbahaa.jpeg"
+
+//MOQAWEL
+export { default as moqwel } from "./20250523_1744_صورة على لابتوب وهاتف_remix_01jvysjhnrfqe8nq3n4zx133va.png"
+export { default as moqwel1 } from "./Screenshot_23-5-2025_173222_muqawil-bright-switch.lovable.app.jpeg"
+export { default as moqwel2} from "./Screenshot_23-5-2025_17356_muqawil-bright-switch.lovable.app.jpeg"
+
+//SECOND PORT
+export { default as proto } from "./Captkjgure.jpeg"
+export { default as proto1 } from "./Screenshot_25-5-2025_141754_myprotofolio.onrender.com.jpeg"
+export { default as proto2 } from "./Screenshot_25-5-2025_14188_myprotofolio.onrender.com.jpeg"
+
+
+export { default as ecom } from "./ecpommers.jpeg"
+export { default as ecom1 } from "./ecom1.jpeg"
+
+
 export {default as elon} from "./elonwebp.webp"
 export {default as hubber} from "./download.webp"
 export {default as grace} from "./grace.webp"
 export {default as gates } from "./billgates.webp"
 export {default as alun } from "./alun.webp"
-export {default as steve} from "./stev.webp"
+export { default as steve } from "./stev.webp"
+
 // export {default as } from "./"
+export {default as sol} from "./sol.png"
+export {default as problem} from "./problem.png"
